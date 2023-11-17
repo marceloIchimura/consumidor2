317d871c1d22a88d4518da3332e98fbfbf6d029d
kazuo <marcelo.ichimura@outlook.com>
2023-11-16 22:40:10 -0300

    feat: commit inicial

---
+++ src/test/java/com/example/consumidor2/Consumidor2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumidor2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Consumidor2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
